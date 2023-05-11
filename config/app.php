ee7e75c105e52a074b6f6ada18b324991cbfe844
Programmer-PC <corplan.ict@gmail.com>
2023-05-11 08:31:25 +0800

    p-pc: fixed yajra/datatables not found

--- config/app.php
+++ config/app.php
@@ -186,7 +186,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        // 'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
 
 ];
